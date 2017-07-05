05330d2fd0b9b24a63ad30b8097515ee1b12a41a
Mohamed Ahmed <mohamed@liberta.nl>
2017-07-05 15:52:45 +0200

    Get plugin name

---
+++ src/Util.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Xel\XWP;
+
+
+class Util {
+
+    /**
+     * Converts a plugin basename back into a friendly slug.
+     * @source https://github.com/wp-cli/wp-cli/blob/master/php/utils-wp.php
+     */
+    public static function get_plugin_name( $basename ) {
+        if ( false === strpos( $basename, '/' ) )
+            $name = basename( $basename, '.php' );
+        else
+            $name = dirname( $basename );
+        return $name;
+    }
+
+}
