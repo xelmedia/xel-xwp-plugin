ffaf59416f42cbdfafa24813cae5ee96a87f3916
Ramom Schriks <ramon@xel.nl>
2017-07-20 12:56:11 +0200

    Added Api-key support
    - Added Authorize method in the Database class to check whether the
      request api-key matches the api-key in the site. If not, it'll die
      with error 401.
    
    - Added constants

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -3,17 +3,39 @@ namespace Xel\XWP\Database;
 
 
 use Xel\XWP\Domain\WpData;
+use Xel\XWP\Rest\RestConstants;
 use Xel\XWP\Util;
 
 class Database implements IDatabase {
 
     /**
+     * This method checks whether the api-key in the request, is equal to the api-key that is set in the WP-site DB.
+     * If not equal, request api-key not set or there is no api-key set for this site, it'll exit using an unauthorized
+     * response (401).
+     * @param $request
+     */
+    private static function authorize($request) {
+        if (array_key_exists(RestConstants::PATH_PARAM_API_KEY, $_REQUEST)) {
+            $apiKeyRequest = $request[RestConstants::PATH_PARAM_API_KEY];
+            $apiKeyResponse = get_option(RestConstants::WP_TABLE_OPTION_API_KEY);
+
+            if($apiKeyResponse && $apiKeyRequest === $apiKeyResponse) {
+                return;
+            }
+        }
+        wp_die(RestConstants::ERROR_MSG_API_KEY, "Unauthorized", ["response" => 401]);
+    }
+
+
+    /**
      * Obtains all the postTypes in the database with a few exclusions.
+     * @param $request -> send from the client containing values such as api_key
      * @return array
      */
-    public static function get_post_types(): array {
-        $postTypes = get_post_types('', 'objects');
+    public static function get_post_types($request): array {
+        self::authorize($request);
 
+        $postTypes = get_post_types('', 'objects');
         $excludePostTypes = ['customize_changeset', 'attachment', 'revision', 'nav_menu_item', 'custom_css'];
 
         $response = [];
@@ -31,9 +53,12 @@ class Database implements IDatabase {
 
     /**
      * Obtains all the Wordpress tables, except the WP-core tables that are regulated by the wpcli commands (i.e postTypes)
+     * @param $request -> send from the client containing values such as api_key
      * @return array
      */
-    public static function get_tables(): array {
+    public static function get_tables($request): array {
+        self::authorize($request);
+
         global $wpdb;
         $excludeTables = ['options', 'users', 'usermeta', 'comments', 'commentmeta', 'termmeta', 'terms', 'term_taxonomy', 'term_relationships', 'posts', 'postmeta' ];
         foreach ($excludeTables as $key => $value) {
@@ -56,9 +81,12 @@ class Database implements IDatabase {
 
     /**
      * Obtains all the plugins which are currently disabled.
+     * @param $request -> send from the client containing values such as api_key
      * @return array
      */
-    public static function get_deactivated_plugins(): array {
+    public static function get_deactivated_plugins($request): array {
+        self::authorize($request);
+
         if ( ! function_exists( 'get_plugins' ) ) {
             require_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
@@ -77,10 +105,13 @@ class Database implements IDatabase {
     }
 
     /**
-     * Obtains all the themes which are currently disabled.
+     * Obtains all the themes which are currently disabled
+     * @param $request -> send from the client containing values such as api_key
      * @return array
      */
-    public static function get_deactivated_themes(): array {
+    public static function get_deactivated_themes($request): array {
+        self::authorize($request);
+
         $wpThemes = wp_get_themes();
         $currentTheme = get_current_theme();
         $response = [];
@@ -98,9 +129,12 @@ class Database implements IDatabase {
 
     /**
      * Obtains all the plugins, regardless of whether they are activated or deactivated.
+     * @param $request -> send from the client containing values such as api_key
      * @return array
      */
-    public static function get_plugins(): array {
+    public static function get_plugins($request): array {
+        self::authorize($request);
+
         if ( ! function_exists( 'get_plugins' ) ) {
             require_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
@@ -118,9 +152,12 @@ class Database implements IDatabase {
 
     /**
      * Obtains all the themes, regardless of whether they are activated or deactivated.
+     * @param $request -> send from the client containing values such as api_key
      * @return array
      */
-    public static function get_themes(): array  {
+    public static function get_themes($request): array  {
+        self::authorize($request);
+
         $wpThemes = wp_get_themes();
         $response = [];
         foreach ($wpThemes as $theme => $value) {
--- src/Database/IDatabase.php
+++ src/Database/IDatabase.php
@@ -6,10 +6,10 @@ namespace Xel\XWP\Database;
 
 
 interface IDatabase {
-    public static function get_post_types();
-    public static function get_tables();
-    public static function get_deactivated_plugins();
-    public static function get_deactivated_themes();
-    public static function get_plugins();
-    public static function get_themes();
+    public static function get_post_types($request);
+    public static function get_tables($request);
+    public static function get_deactivated_plugins($request);
+    public static function get_deactivated_themes($request);
+    public static function get_plugins($request);
+    public static function get_themes($request);
 }
