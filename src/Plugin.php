5ffad1fb23f392f69fb0e17dbe1c7267e92c4873
Mo Ahmed <mohamed@liberta.nl>
2019-09-11 17:31:39 +0200

    Require authorization only for xel-xwp namespace

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,15 +5,10 @@ use Xel\XWP\Rest\RestConstants;
 use Xel\XWP\Rest\RestRoute;
 
 class Plugin {
+    const ENDPOINT_NAMESPACE = "xel-xwp/v1";
 
     public function __construct() {
-        self::xel_manage_filters();
         add_action('rest_api_init', array(__CLASS__ , 'xel_rest_init'));
-    }
-
-    private static function xel_manage_filters() {
-        add_filter('automatic_updater_disabled', '__return_true');
-        add_filter('auto_core_update_send_email', '__return_false');
     }
 
     private static function xel_header_add() {
@@ -21,10 +16,7 @@ class Plugin {
     }
 
     private static function xel_rest_add(RestRoute $route) {
-        $version = 'v1';
-        $namespace = 'xel-xwp/' . $version;
-
-        register_rest_route( $namespace, $route->getPathUri(), array(
+        register_rest_route(self::ENDPOINT_NAMESPACE, $route->getPathUri(), array(
             'methods'  => $route->getRequestType(),
             'callback' =>  array($route->getClassPath(), $route->getMethodName())
         ) );
@@ -34,10 +26,13 @@ class Plugin {
      * Authorization method that checks whether the api-key is valid.
      * If not equal, request api-key not set or there is no api-key set for this site, it'll exit using an unauthorized
      * response (401).
-     * @return bool || WP_ERROR
+     * @return bool || WP_ERROR || null
+     *
      */
     public static function xel_authorize() {
-        if (array_key_exists(RestConstants::PATH_PARAM_API_KEY, $_REQUEST)) {
+        if(strpos($_SERVER['REQUEST_URI'], self::ENDPOINT_NAMESPACE) === false) return null;
+
+        if (isset($_REQUEST[RestConstants::PATH_PARAM_API_KEY])) {
             $apiKeyRequest = $_REQUEST[RestConstants::PATH_PARAM_API_KEY];
             $apiKeyResponse = get_option(RestConstants::WP_TABLE_OPTION_API_KEY);
 
